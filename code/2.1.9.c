ed7f86dbcd2549824676ea82f31628dffd68678c
lijie169 <258595660@qq.com>
2015-06-07 10:41:51 +0800

    the difference between strncpy and memcpy
    事出反常必有妖，肯定有原因的。

--- code/2.1.9.c
+++ code/2.1.9.c
@@ -1,9 +1,11 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<string.h>
 #include<time.h>
-
+typedef char*  Ptr ;
 #define ARR_LEN 10
 void print(int *arr ,int len);
+typedef void (*com_fun)(void);
 size_t shellsort(int *arr,size_t len)
 {
     size_t h = 1,j,i,count = 0;
@@ -52,7 +54,7 @@ void insert_sort_index(int *arr,size_t len)
         }
         arr[j+1]= temp;
 
-        print(arr,ARR_LEN);
+       // print(arr,ARR_LEN);
     }
 }
 void generate(int *arr ,int len)
@@ -113,11 +115,70 @@ void test_insert_sort(void)
     printf("---------------------------------------\n");
     print(arr,ARR_LEN);
 }
+
+double run_time(com_fun fun)
+{
+	time_t start,end ;
+	double diff ;
+	start = time(NULL);
+	fun();
+	end = time(NULL);
+	diff = difftime(end,start);
+	return diff ;
+}
+
+void test_3_sort_time(void)
+{
+	#define TEST_LEN 100000
+	int arr[3][TEST_LEN];
+	time_t start,end ;
+	double diff1,diff2,diff3 ;
+	generate(arr[0],TEST_LEN);
+
+	memcpy((void*)arr[1],(void*)arr[0],sizeof(arr[0]));
+	memcpy((void*)arr[2],(void*)arr[0],sizeof(arr[0]));
+
+
+	start = time(NULL);
+	insert_sort(arr[0],TEST_LEN);
+	end = time(NULL);
+	diff1 = difftime(end,start);
+
+	start = time(NULL);
+	insert_sort_index(arr[1],TEST_LEN);
+	end = time(NULL);
+	diff2 = difftime(end,start);
+
+	start = time(NULL);
+	shellsort(arr[2],TEST_LEN);
+	end = time(NULL);
+	diff3 = difftime(end,start);
+
+	printf("%f,%f,%f\n",diff1,diff2,diff3);
+	if(memcmp((void*)arr[0],(void*)arr[1],sizeof(arr[0])))
+	{
+		printf("sort err\n");
+	}
+	if(memcmp((void*)arr[0],(void*)arr[1],sizeof(arr[0])))
+	{
+		printf("sort err 2\n");
+	}
+
+}
+
+void test(void )
+{
+  char str[] ={0,'a','b','c'};
+  char dst[20]="abcdefg";
+  strncpy(dst,str,10);
+  printf("%s\n",dst);
+}
 int main(void)
 {
 
     //test_shellsort();
     //test_insert_sort();
-    test_insert_sort_index();
-
+    //test_insert_sort_index();
+    //test_3_sort_time();
+    test();
 }
